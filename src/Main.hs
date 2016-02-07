fea144b04c82e8d87f2e239ee399eedc9f177ae4
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-02-07 16:48:25 +0200

    Boolean examples moved to the separate file

--- src/Main.hs
+++ src/Main.hs
@@ -2,29 +2,4 @@ module Main where
 
 main :: IO ()
 
-main = putStr "dasdasd"
-
-myTrue :: Bool
-myTrue = True
-
-myFalse :: Bool
-myFalse = False
-
-myNot :: Bool -> Bool
-myNot True = False
-myNot False = True
-
-myAnd :: Bool -> Bool -> Bool
-myAnd False _ = False
-myAnd True x = x
-
-myOr :: Bool -> Bool -> Bool
-myOr True _ = True
-myOr False x = x
-
-myXor :: Bool -> Bool -> Bool
-myXor a b = myOr (myAnd (myNot a) b) (myAnd a (myNot b))
-
-myIfThenElse :: Bool -> a -> a -> a
-myIfThenElse True t _ = t
-myIfTherElse False _ e = e
+main = putStr "Hello Haskell!"
