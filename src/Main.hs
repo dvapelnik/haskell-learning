e16d5f8ca4223dfa5b8bc31a82fcd31a25191dc5
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-02-07 17:23:21 +0200

    Calculating integer values

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,13 @@
 module Main where
 
+import MyBoolean
+import Calculus
+
 main :: IO ()
 
-main = putStr "Hello Haskell!"
+main = do
+    putStrLn ( show $ add 2 2)
+    putStrLn ( show $ diff 1 5)
+    putStrLn ( show $ division 1 2 )
+    putStrLn ( show $ multiple 2 4 )
+    putStrLn ( show $ power 2 10 )
