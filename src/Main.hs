8a58b79d6930c8b704d146204e50333caddba231
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-02-08 21:11:52 +0200

    Using simple real clean functions with definitions by mathing and guarding and using let xor where

--- src/Main.hs
+++ src/Main.hs
@@ -3,14 +3,33 @@ module Main where
 import MyBoolean
 import Calculus
 import ClassOfGroup
+import RealFunctions
 
 main :: IO ()
 
 main = do
+    putStrLn "- Calculus"
     putStrLn ( show $ add 2 2)
     putStrLn ( show $ diff 1 5)
     putStrLn ( show $ division 1 2 )
     putStrLn ( show $ multiple 2 4 )
     putStrLn ( show $ power 2 10 )
+    putStrLn "- Lazying"
     print(take 2 (replicate 100 "127.0.0.1"))
     print(take 4 (repeat "127.0.0.1"))
+    putStrLn "- Real clean functions"
+    putStrLn "-- Double me int"
+    print (doubleMeInt 4)
+    print (doubleMeInt 49)
+    putStrLn "-- IP resolving"
+    putStrLn (resolveIpIfThenElse "127.0.0.1")
+    putStrLn (resolveIpIfThenElse "10.1.16.1")
+    putStrLn (resolveIpMatched "127.0.0.1")
+    putStrLn (resolveIpMatched "10.1.16.1")
+    putStrLn (resolveIpGuarded "127.0.0.1")
+    putStrLn (resolveIpGuarded "10.1.16.1")
+    putStrLn (resolveIpGuarded "")
+    putStrLn "-- Length converting"
+    print (lengthConversionSimple 1.5)
+    print (lengthConversionWithWhere 1.5)
+    print (lengthConversionWithLet 1.5)
---
+++ src/RealFunctions.hs
@@ -0,0 +1,41 @@
+module RealFunctions where
+
+doubleMeInt :: Int -> Int
+doubleMeInt a = a + a
+
+-- BEGIN OF "IP RESOLVING"
+resolveIpIfThenElse :: String -> String
+resolveIpIfThenElse address =
+    if address == "127.0.0.1" then "localhost" else address
+
+resolveIpMatched :: String -> String
+resolveIpMatched "127.0.0.1" = "localhost"
+resolveIpMatched address = address
+
+resolveIpGuarded :: String -> String
+resolveIpGuarded address
+    | address == "127.0.0.1" = "localhost"
+    | "" == address = "Empty IP"
+    | null address  = "Empty IP"
+    | otherwise = address
+-- END OF "IP RESOLVING"
+
+-- BEGIN OF "LENGTH CONVERTING"
+lengthConversionSimple :: Double -> Double
+lengthConversionSimple len = len * 0.8764 - 0.24
+
+lengthConversionWithWhere :: Double -> Double
+lengthConversionWithWhere len =
+    len * coeff - delta
+    where
+        coeff = 0.8764
+        delta = 0.24
+
+lengthConversionWithLet :: Double -> Double
+lengthConversionWithLet len =
+    let
+        coeff = 0.8764
+        delta = 0.24
+    in
+    len * coeff - delta
+-- END OF "LENGTH CONVERTING"
