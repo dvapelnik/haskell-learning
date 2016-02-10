41a0c6e8f08dd11b239fb1115c35073009247153
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-02-10 03:23:30 +0200

    Functional chains

--- src/Main.hs
+++ src/Main.hs
@@ -1,53 +1,30 @@
 module Main where
 
-import L_00_MyBoolean
-import L_01_Calculus
-import L_02_ClassOfGroup
-import L_03_RealFunctions
-import L_04_Lambdas
-import L_05_Types
-import L_06_PartialApplying
-import L_07_FunctionAsArgument
+import Data.Char
+import Data.List
+import Data.String.Utils
 
-main :: IO ()
+x |> f = f x
+
+addPrefix :: String -> String
+addPrefix url =
+    if prefix `isPrefixOf` url then url else prefix ++ url
+--    Also also may be uses as
+--    if prefix `isPrefixOf` url then url else prefix ++ url
+    where prefix = "http://"
+
+encodeAllSpaces = replace " " "%20"
+
+makeItLowerCase = map toLower
 
 main = do
-    putStrLn "- Calculus"
-    putStrLn ( show $ add 2 2)
-    putStrLn ( show $ diff 1 5)
-    putStrLn ( show $ division 1 2 )
-    putStrLn ( show $ multiple 2 4 )
-    putStrLn ( show $ power 2 10 )
-    putStrLn "- Lazying"
-    print(take 2 (replicate 100 "127.0.0.1"))
-    print(take 4 (repeat "127.0.0.1"))
-    putStrLn "- Real clean functions"
-    putStrLn "-- Double me int"
-    print (doubleMeInt 4)
-    print (doubleMeInt 49)
-    putStrLn "-- IP resolving"
-    putStrLn (resolveIpIfThenElse "127.0.0.1")
-    putStrLn (resolveIpIfThenElse "10.1.16.1")
-    putStrLn (resolveIpMatched "127.0.0.1")
-    putStrLn (resolveIpMatched "10.1.16.1")
-    putStrLn (resolveIpGuarded "127.0.0.1")
-    putStrLn (resolveIpGuarded "10.1.16.1")
-    putStrLn (resolveIpGuarded "")
-    putStrLn "-- Length converting"
-    print (lengthConversionSimple 1.5)
-    print (lengthConversionWithWhere 1.5)
-    print (lengthConversionWithLet 1.5)
-    putStrLn "- Lambdas"
-    print ((\x -> x * x ) 5)
-    print (powerLambda 2 3)
-    putStrLn "- Types"
-    print(doubleMeIntTyped 45)
-    putStrLn "- Partial applying"
-    print(doubleSummaInt 1 2)
-    print((doubleSummaInt 1) 2)
-    putStrLn "- Partial applying with let"
-    print((\x y -> let tmp_fn = doubleSummaInt x in tmp_fn y) 2 3)
-    putStrLn "- Actor executor"
-    print(let actor = \x y -> x + y in actor_executor actor 2 3)
-    print(let actor = \x y -> x - y in actor_executor actor 2 3)
-    print(let actor = \x y -> x * y - y + 2 * x in actor_executor actor 2 3)
+    putStrLn (addPrefix(encodeAllSpaces(makeItLowerCase url)))
+    -- function composition
+    putStrLn ((addPrefix . encodeAllSpaces . makeItLowerCase) url)
+    -- function application
+    putStrLn (addPrefix $ encodeAllSpaces $ makeItLowerCase url)
+    -- function composition combined with function application
+    putStrLn $ addPrefix . encodeAllSpaces . makeItLowerCase $ url
+    -- also with function 'x |> f = f x' as
+    url |> makeItLowerCase |> encodeAllSpaces |> addPrefix |> putStrLn
+    where url = "www.SITE.com/test me/Start page"
