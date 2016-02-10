39ca359ac7cac228b629964d177cf8cea9621490
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-02-11 00:35:30 +0200

    Using functions on infix form and oprators in prefix form

--- src/Main.hs
+++ src/Main.hs
@@ -1,30 +1,13 @@
 module Main where
 
-import Data.Char
 import Data.List
 import Data.String.Utils
 
-x |> f = f x
-
-addPrefix :: String -> String
-addPrefix url =
-    if prefix `isPrefixOf` url then url else prefix ++ url
---    Also also may be uses as
---    if prefix `isPrefixOf` url then url else prefix ++ url
-    where prefix = "http://"
-
-encodeAllSpaces = replace " " "%20"
-
-makeItLowerCase = map toLower
-
 main = do
-    putStrLn (addPrefix(encodeAllSpaces(makeItLowerCase url)))
-    -- function composition
-    putStrLn ((addPrefix . encodeAllSpaces . makeItLowerCase) url)
-    -- function application
-    putStrLn (addPrefix $ encodeAllSpaces $ makeItLowerCase url)
-    -- function composition combined with function application
-    putStrLn $ addPrefix . encodeAllSpaces . makeItLowerCase $ url
-    -- also with function 'x |> f = f x' as
-    url |> makeItLowerCase |> encodeAllSpaces |> addPrefix |> putStrLn
-    where url = "www.SITE.com/test me/Start page"
+    print $ (++) "127.0.0.1" ":8080"
+    print $ "127.0.0.1" ++ ":8080"
+    print $ (+) 1 2
+    print $ 1 + 2
+    print $ if ".com" `isSuffixOf` "google.com" then ".com is suffix of google.com" else "isn't"
+    print $ if isSuffixOf ".com" "google.com" then ".com is suffix of google.com" else "isn't"
+
