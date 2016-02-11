13f22c4a15dc57eb8947969ddfc671a7d24de29d
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-02-11 18:03:34 +0200

    List examples

--- src/Main.hs
+++ src/Main.hs
@@ -3,11 +3,74 @@ module Main where
 import Data.List
 import Data.String.Utils
 
-main = do
-    print $ (++) "127.0.0.1" ":8080"
-    print $ "127.0.0.1" ++ ":8080"
-    print $ (+) 1 2
-    print $ 1 + 2
-    print $ if ".com" `isSuffixOf` "google.com" then ".com is suffix of google.com" else "isn't"
-    print $ if isSuffixOf ".com" "google.com" then ".com is suffix of google.com" else "isn't"
+makeDaysWithPrefix :: String -> [String]
+makeDaysWithPrefix prefix = [prefix ++ space ++ "Sunday", prefix ++ space ++ "Monday"]
+    where space = " "
 
+main = do
+    print $ makeDaysWithPrefix "This day is"
+    print $
+        let
+            space = " "
+            appender = \x -> "Dear" ++ space ++ x
+        in
+        map appender ["John","Anna","Andrew"]
+    print $
+        let
+            dear_appender = (\x y -> x ++ " " ++ y) "Dear"
+        in
+        map dear_appender ["John", "Anna", "Andrew"]
+    print $
+        let
+            space = " "
+            appender = \x -> "Dear" ++ space ++ x
+        in
+        map appender ["John", "Anna", "Andrew"]
+    print $
+        let
+            space = " "
+            appender = \x -> "Dear" ++ space ++ x
+            lst = map appender ["John", "Anna", "Andrew"]
+        in length lst
+    print $
+        let
+            earth_planet_name = "Earth"
+            isPlanet = \x -> x `elem` ["Earth", "Jupiter"]
+        in
+        if isPlanet earth_planet_name
+            then "Yes, " ++ earth_planet_name ++ " is a planet"
+            else "No, " ++ earth_planet_name ++ " is not a planet"
+    print $
+        let
+            addNewHostToFront = \x y -> x : y
+            hosts = ["45.67.78.89", "123.45.65.54", "127.0.0.1"]
+        in
+        addNewHostToFront "124.67.54.90" hosts
+    print $
+        let
+            addNewHostToBack = \x y -> y ++ [x]
+            hosts = ["45.67.78.89", "123.45.65.54", "127.0.0.1"]
+        in
+        addNewHostToBack "124.67.54.90" hosts
+    print $
+        let
+            notEmptyNameFilter = not . null
+            removeAllEmptyNamesFrom = \x -> filter notEmptyNameFilter x
+        in
+        removeAllEmptyNamesFrom ["John", "", "Ann"]
+    print $
+        let
+            removeAllEmptyNamesFrom = \x -> filter (not . null) x
+        in
+        removeAllEmptyNamesFrom ["John", "", "Ann"]
+    print $ foldl (++) "http" ["://", "www", ".", "google.com"]
+    print $ scanl (++) "http" ["://", "www", ".", "google.com"]
+    print $ foldl (++) "" ["http", "://", "www", ".", "google.com"]
+    print $ foldl1 (++) ["http", "://", "www", ".", "google.com"]
+    print $ foldr (++) ".ua" ["http", "://", "www", ".", "google.com"]
+    print $ scanr (++) ".ua" ["http", "://", "www", ".", "google.com"]
+    print $
+        let
+            proto = "https"
+        in
+        foldr (++) "" [proto, "://", "www", ".", "google.com"]
