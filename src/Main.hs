a81d664e5afb3bf5aa02ff9b065bba1dded34811
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-02-12 16:56:32 +0200

    Tuuples

--- src/Main.hs
+++ src/Main.hs
@@ -2,20 +2,37 @@ module Main where
 
 import Data.List
 import Data.String.Utils
+import Data.Tuple.Extra
+import Data.Tuple.Select
+
+chessMoveFstSec :: (String, String) -> String
+chessMoveFstSec pair = fst pair ++ "-" ++ snd pair
+
+chessMovePatternMatching :: (String, String) -> String
+chessMovePatternMatching (f, s) = f ++ "-" ++ s
+
+get31 :: (String, String, String) -> String
+get31 (elem, _, _) = elem
+
+get32 :: (String, String, String) -> String
+get32 (_, elem, _) = elem
+
+get33 :: (String, String, String) -> String
+get33 (_, _, elem) = elem
+
 
 main = do
-    print $ [1..10]
-    print $ ['a'..'z']
-    print $ ['а'..'я']
-    print $ [2,4..10]
-    print $ [1.1,1.2..2]
-    print $ [120,110..10]
-    print $ take 5 [1..]
-    print $
-        let
-            begin = 2
-            delta = 5
-        in
-        take 5 [begin, begin + delta..]
-
-
+    print "Parallel composition opetaror"
+    print (succ *** reverse  $(2, "text"))
+    print "Fst ans Sec of pair"
+    print $ chessMoveFstSec ("e2", "e4")
+    print "Pattern matching using"
+    print $ chessMovePatternMatching ("e2", "e4")
+    print "Own getNI"
+    print $ get31 ("One", "Two", "Three")
+    print $ get32 ("One", "Two", "Three")
+    print $ get33 ("One", "Two", "Three")
+    print "selI from select package"
+    print $ sel1 ("One", "Two", "Three")
+    print $ sel2 ("One", "Two", "Three")
+    print $ sel3 ("One", "Two", "Three")
