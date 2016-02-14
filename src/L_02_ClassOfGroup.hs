b98b2853c78705396863258fa0f5726e815f4a2c
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-02-14 15:20:44 +0200

    Lesson files removed (cherry picked from commit c822d89)

--- src/L_02_ClassOfGroup.hs
+++
@@ -1,5 +0,0 @@
-module L_02_ClassOfGroup where
-
-class MyGroup a where
-    e   :: a
-    inv :: a -> a
--- src/L_05_Types.hs
+++
@@ -1,6 +0,0 @@
-module L_05_Types where
-
-type IntToInt = Int -> Int
-
-doubleMeIntTyped :: IntToInt
-doubleMeIntTyped a = a + a
