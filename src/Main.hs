e757c16a1aca97fb7b63a218cbfaac8157224906
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-02-14 22:34:41 +0200

    Function with polymorphic argument

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,17 @@
 module Main where
 
-isPalindrome :: (Eq a) => [a] -> Bool
-isPalindrome [] = True
-isPalindrome [_] = True
-isPalindrome xs = xs == (reverse xs)
+import Text.Printf
 
-isPalindrome' :: (Eq a) => [a] -> Bool
-isPalindrome' [] = True
-isPalindrome' [_] = True
-isPalindrome' xs = (head xs) == (last xs) && (isPalindrome' . init . tail $ xs)
+data Person = Male String Int | Female String Int
+
+instance Show Person where
+    show (Male   name age) = printf "I'm a male. My name is '%s' and my age is '%d'"   name age
+    show (Female name age) = printf "I'm a female. My name is '%s' and my age is '%d'" name age
+
+stringifyPerson :: Person -> String
+stringifyPerson person = show person
 
 main = do
-    print $ isPalindrome    "ama"
-    print $ isPalindrome    "amaa"
-    print $ isPalindrome'   "ama"
-    print $ isPalindrome'   "amaa"
+    print . stringifyPerson $ (Male   "Peter" 28)
+    print . stringifyPerson $ (Female "Vikky" 23)
+
