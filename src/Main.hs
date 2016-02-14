6a5d8e60ad0c21a054233f3ebee13a95e4e27cee
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-02-14 21:25:21 +0200

    Value and nullary constructors

--- src/Main.hs
+++ src/Main.hs
@@ -7,9 +7,21 @@ instance Show IpAddress where
         then "localhost"
         else address
 
-data Person = Person String Int
+data Person = Person String Int | PersonNamed String Int
 instance Show Person where
-    show (Person name age) = "Person has name '" ++ name ++ "' and age '" ++ show age ++ "'"
+    show (Person name age)      =   "Person has name '" ++ name ++ "' and age '" ++ show age ++ "'"
+    show (PersonNamed name _)   =   "PersonNamed has name '" ++ name ++ "'"
+
+data TransportLayer = TCP | UDP | SCTP | DCCP | SPX
+
+descriptionOf :: TransportLayer -> String
+descriptionOf proto =
+    case proto of
+        TCP  -> "Transmission Control Protocol"
+        UDP  -> "User Datagram Protocol"
+        SCTP -> "Stream Control Transmission Protocol"
+        DCCP -> "Datagram Congestion Control Protocol"
+        SPX  -> "Sequenced Packet Exchange"
 
 main = do
     print $
@@ -22,3 +34,13 @@ main = do
             peter = Person "Peter" 28
         in
         show peter
+    print $
+        let
+            peter = PersonNamed "Peter" 28
+        in
+        show peter
+    print $ descriptionOf TCP
+    print $ descriptionOf UDP
+    print $ descriptionOf SCTP
+    print $ descriptionOf DCCP
+    print $ descriptionOf SPX
