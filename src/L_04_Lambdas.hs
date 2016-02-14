c822d89c03e710fb523e188d3a1fa4d72a1c4378
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-02-14 15:20:44 +0200

    Lesson files removed

--- src/L_04_Lambdas.hs
+++
@@ -1,6 +0,0 @@
-module L_04_Lambdas where
-
-powerLambda :: Int -> Int -> Int
-powerLambda a b =
-    f a b
-    where f = \arg1 arg2 -> arg1 ^ arg2
--- src/L_06_PartialApplying.hs
+++
@@ -1,4 +0,0 @@
-module L_06_PartialApplying where
-
-doubleSummaInt :: Int -> Int -> Int
-doubleSummaInt a b  = a + b
