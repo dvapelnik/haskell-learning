c5457b1eefd7edaa231ea0fe99aa10fc89c4e813
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-02-14 02:21:05 +0200

    Get but last element of list

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,21 @@
 module Main where
 
-myLastRecursive :: [a] -> a
-myLastRecursive [] = error "List is empty!"
-myLastRecursive [x] = x
-myLastRecursive (_:x) = myLastRecursive x
+myButLastLastInit :: [a] -> a
+myButLastLastInit [] = error "List is empty"
+myButLastLastInit [_] = error "Has only one element"
+myButLastLastInit x = last . init $ x
 
-myLastByIndex :: [a] -> a
-myLastByIndex [] = error "List is empty!"
-myLastByIndex x = x !! (length x - 1)
+myButLastByIndex :: [a] -> a
+myButLastByIndex [] = error "List is empty"
+myButLastByIndex [_] = error "Has only one element"
+myButLastByIndex x = x !! (length x - 2)
+
+myButLastSecondAfterReverse :: [a] -> a
+myButLastSecondAfterReverse [] = error "List is empty"
+myButLastSecondAfterReverse [_] = error "Has only one element"
+myButLastSecondAfterReverse x = reverse x !! 1
 
 main = do
-    print $ last [1, 2, 3]
-    print . head . reverse $  [1, 2, 3]
-    print . myLastRecursive $ take 10 [1,19..]
-    print . myLastByIndex $ take 10 [1,19..]
+    print $ myButLastLastInit [1, 2, 3]
+    print $ myButLastByIndex [1, 2, 3]
+    print $ myButLastSecondAfterReverse [1, 2, 3]
