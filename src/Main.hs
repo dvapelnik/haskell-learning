76cb61a1728fc3eae5d34167b4eca17901365211
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-02-14 16:28:37 +0200

    List reversing

--- src/Main.hs
+++ src/Main.hs
@@ -1,26 +1,13 @@
 module Main where
 
-myLength :: [a] -> Int
-myLength [] = 0
-myLength [_] = 1
-myLength x = length x
+myReverse :: String -> String
+myReverse s = reverse s
 
-myLength' :: [a] -> Int
-myLength' [] = 0
-myLength' [_] = 1
-myLength' (_:xs) = 1 + myLength' xs
+myReverse' :: String -> String
+myReverse' [] = []
+myReverse' (x:xs) = myReverse' xs ++ [x]
 
 main = do
-    print $ myLength []
-    print $ myLength [1]
-    print $ myLength [1..3]
-    print $ myLength' []
-    print $ myLength' [1]
-    print $ myLength' [1..3]
-    print $ foldl (\n _ -> n + 1) 0 [1..4]
-    print $ foldr (\_ n -> n + 1) 0 [1..5]
-    print $ foldr (\_ -> (+1)) 0 [1..6]
-    print $ foldr ((+) . (const 1)) 0 [1..7]
-    print $ foldr (const (+1)) 0 [1..8]
-    print $ foldl (const . (+1)) 0 [1..9]
-    print . sum $ map (\_ -> 1) [1..10]
+    print $ myReverse   "A man, a plan, a canal, panama!"
+    print $ myReverse'  "A man, a plan, a canal, panama!"
+    print $ foldl (flip (:)) [] "A man, a plan, a canal, panama!"
