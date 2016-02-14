013e5853b53dd4183d8246167092e95a6afaf91f
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-02-14 15:47:14 +0200

    Git list's length

--- src/Main.hs
+++ src/Main.hs
@@ -1,49 +1,26 @@
 module Main where
 
-elementAt :: [a] -> Int -> a
-elementAt   [] _    = error "List is empty"
-elementAt   [x] 1   = x
-elementAt   x n     =
-    if n > length x
-    then error "Out of bounds"
-    else x !! (n - 1)
+myLength :: [a] -> Int
+myLength [] = 0
+myLength [_] = 1
+myLength x = length x
 
-elementAt' :: [a] -> Int -> a
-elementAt'   [] _    = error "List is empty"
-elementAt'   [x] 1   = x
-elementAt'   x n
-    | n > length x  = error "Out of bounds"
-    | otherwise     = x !! (n - 1)
-
-elementAt'' :: [a] -> Int -> a
-elementAt'' (x:_) 1 = x
-elementAt'' [] _    = error "Out of bounds"
-elementAt'' (_:xs) k
-    | k < 1         = error "Out of bounds"
-    | otherwise     = elementAt'' xs (k - 1)
-
-elementAt''' :: [a] -> Int -> a
-elementAt''' (x:_) 1    = x
-elementAt''' [] _       = error "Out of bounds"
-elementAt''' (_:xs) k   = elementAt''' xs (k - 1)
-elementAt''' _ _        = error "Out of bounds"
-
-elementAt'''' :: [a] -> Int -> a
-elementAt'''' x n
-    | length x == 0     = error "List is empty"
-    | length x < n      = error "Out of bounds"
-    | otherwise         = fst . last $ zip x [1..n]
-
-elementAt''''' :: [a] -> Int -> a
-elementAt''''' x n
-    | length x == 0     = error "List is empty"
-    | length x < n      = error "Out of bounds"
-    | otherwise         = head $ drop (n - 1) x
+myLength' :: [a] -> Int
+myLength' [] = 0
+myLength' [_] = 1
+myLength' (_:xs) = 1 + myLength' xs
 
 main = do
-    print $ elementAt       [1..3] 1
-    print $ elementAt'      [1..3] 2
-    print $ elementAt''     [1..3] 3
-    print $ elementAt'''    [1..3] 1
-    print $ elementAt''''   [1..3] 2
-    print $ elementAt'''''  [1..3] 3
+    print $ myLength []
+    print $ myLength [1]
+    print $ myLength [1..3]
+    print $ myLength' []
+    print $ myLength' [1]
+    print $ myLength' [1..3]
+    print $ foldl (\n _ -> n + 1) 0 [1..4]
+    print $ foldr (\_ n -> n + 1) 0 [1..5]
+    print $ foldr (\_ -> (+1)) 0 [1..6]
+    print $ foldr ((+) . (const 1)) 0 [1..7]
+    print $ foldr (const (+1)) 0 [1..8]
+    print $ foldl (const . (+1)) 0 [1..9]
+    print . sum $ map (\_ -> 1) [1..10]
