3232f16f8b170e44fd207ac75a35cfb1e3578938
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-02-15 23:24:18 +0200

    Filtering polymorphic types inside list generator

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Text.Printf
 
-data Person = Male String Int | Female String Int
+data Person = Male {name::String, age::Int} | Female {name::String, age::Int}
 
 instance Show Person where
     show (Male   name age) = printf "I'm a male. My name is '%s' and my age is '%d'"   name age
@@ -14,4 +14,11 @@ stringifyPerson person = show person
 main = do
     print . stringifyPerson $ (Male   "Peter" 28)
     print . stringifyPerson $ (Female "Vikky" 23)
+    print $ [person | person@(Male name age) <- persons]
+    print $ [person | person@(Female name age) <- persons]
+    print $ [name person | person@(Male {}) <- persons]
+    print $ [age person | person@(Male {}) <- persons]
+    print $ [name person ++ " has age " ++ show (age person) | person@(Male {}) <- persons]
+    print $ [name person ++ " has age " ++ show (age person) | person@(Female {}) <- persons]
+        where persons = [Male "Peter" 28, Male "Nick" 20, Female "Jennny" 25]
 
