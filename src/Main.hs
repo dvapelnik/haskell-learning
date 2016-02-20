29eee0ec47b6bd1184a5f870cfd9a1273ca8c25a
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-02-20 23:21:17 +0200

    Use context in class

--- src/Main.hs
+++ src/Main.hs
@@ -8,6 +8,9 @@ class Pepper p where
     color :: p -> String
     pungency :: p -> SHU
 
+class Pepper p => Chili p where
+    kind :: p -> String
+
 data Poblano = Poblano
 data TrinidadScorpion = TrinidadScorpion
 
@@ -19,14 +22,32 @@ instance Pepper TrinidadScorpion where
     color TrinidadScorpion = "red"
     pungency TrinidadScorpion = 855000
 
+instance Chili Poblano where
+    kind Poblano = "Chili poblano"
+
+instance Chili TrinidadScorpion where
+    kind TrinidadScorpion = "Chili trinidad scorpion"
+
+-- But not this:
+--data SomeOtherType = SomeOtherType
+--instance Chili SomeOtherType where
+--    kind SomeOtherType = "Not works"
+
+pepperInfo :: Pepper pepper => pepper -> String
+pepperInfo pepper = show (pungency pepper) ++ ", " ++ color pepper
+
 main = do
-    printf "Pepper 'Poblano' has %s color and %s pungency" poblanoColorString poblanoPungencyString
-    printf "Pepper 'Poblano' has %s color and %s pungency" trinidadColorString trinidadPungencyString
+    putStrLn $
+        printf "Pepper 'Poblano' has %s color and %s pungency. %s" poblanoColorString poblanoPungencyString poblanoKindString
+    putStrLn $
+        printf "Pepper 'Poblano' has %s color and %s pungency. %s" trinidadColorString trinidadPungencyString trinidadKindString
     where
         poblano = Poblano
         poblanoColorString = color poblano
         poblanoPungencyString = show (pungency poblano)
+        poblanoKindString = kind poblano
 
         trinidad = TrinidadScorpion
         trinidadColorString = color trinidad
         trinidadPungencyString = show (pungency trinidad)
+        trinidadKindString = kind trinidad
