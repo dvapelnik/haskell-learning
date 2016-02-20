5c810b20b48bbcff19db36f3ea01d133eee3a62e
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-02-21 00:27:30 +0200

    Class constants (like a static fields)

--- src/Main.hs
+++ src/Main.hs
@@ -5,49 +5,24 @@ import Text.Printf
 type SHU = Integer
 
 class Pepper p where
-    color :: p -> String
+    simple   :: p           -- Это константное значение, а не функция.
+    color    :: p -> String
     pungency :: p -> SHU
+    name     :: p -> String
 
-class Pepper p => Chili p where
-    kind :: p -> String
-
-data Poblano = Poblano
-data TrinidadScorpion = TrinidadScorpion
+data Poblano = Poblano String
 
 instance Pepper Poblano where
-    color Poblano = "green"
-    pungency Poblano = 1500
-
-instance Pepper TrinidadScorpion where
-    color TrinidadScorpion = "red"
-    pungency TrinidadScorpion = 855000
-
-instance Chili Poblano where
-    kind Poblano = "Chili poblano"
-
-instance Chili TrinidadScorpion where
-    kind TrinidadScorpion = "Chili trinidad scorpion"
-
--- But not this:
---data SomeOtherType = SomeOtherType
---instance Chili SomeOtherType where
---    kind SomeOtherType = "Not works"
-
-pepperInfo :: Pepper pepper => pepper -> String
-pepperInfo pepper = show (pungency pepper) ++ ", " ++ color pepper
+    simple = Poblano "ancho"         -- Готовим простое значение.
+    color (Poblano name) = "green"
+    pungency (Poblano name) = 1500
+    name (Poblano name) = name
 
 main = do
-    putStrLn $
-        printf "Pepper 'Poblano' has %s color and %s pungency. %s" poblanoColorString poblanoPungencyString poblanoKindString
-    putStrLn $
-        printf "Pepper 'Poblano' has %s color and %s pungency. %s" trinidadColorString trinidadPungencyString trinidadKindString
+    print $ name (simple :: Poblano)
+    print $ color poblano
+    print $ pungency poblano
+    print $ name poblano
     where
-        poblano = Poblano
-        poblanoColorString = color poblano
-        poblanoPungencyString = show (pungency poblano)
-        poblanoKindString = kind poblano
+        poblano = Poblano "POBLANO"
 
-        trinidad = TrinidadScorpion
-        trinidadColorString = color trinidad
-        trinidadPungencyString = show (pungency trinidad)
-        trinidadKindString = kind trinidad
