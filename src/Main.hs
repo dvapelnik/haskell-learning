e93fbc87df8d1a0c0884c4c3de2c893ae017ec52
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-02-24 23:29:56 +0200

    Using do-notation inside function with action for instance IO String

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,14 @@
 module Main where
 
+promptString :: String -> IO String
+promptString prompt = do
+    putStrLn prompt
+    getLine
+
+data User = User { firstname, lastname :: String } deriving (Show)
+
 main :: IO ()
 main = do
-    putStrLn $ "Input your text, please:"
-    lineFromUser <- getLine
-    putStrLn $ "Not bad: " ++ lineFromUser
+    firstname <- promptString "Input first name:"
+    lastname <- promptString "Input last name:"
+    print $ User { firstname = firstname, lastname = lastname}
