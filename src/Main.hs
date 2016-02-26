318858d5a59e4409f1055855bbfee0c29164386b
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-02-26 19:43:11 +0200

    use 'catch' and 'handle' function for catching errors. 'catch' for infix syntax and 'handle' for suffix syntax

--- src/Main.hs
+++ src/Main.hs
@@ -4,9 +4,29 @@ import System.IO
 import Control.Exception
 import System.Environment
 
+--catch :: Exception e => IO a -> (e -> IO a) -> IO a
+
+tryToOpenFile :: FilePath -> IO String
+tryToOpenFile path =
+    readFile path `catch` possibleErrors
+--    catch (readFile path) possibleErrors
+    where
+        possibleErrors :: IOException -> IO String
+        possibleErrors error = return $ show error
+
+tryToOpenFile' :: FilePath -> IO String
+tryToOpenFile' path =
+    handle possibleErrors (readFile path)
+    where
+        possibleErrors :: IOException -> IO String
+        possibleErrors error = return $ show (error) ++ " with handling"
+
 main :: IO ()
 main = do
     homePath <- getEnv "HOME"
-    putStrLn homePath
-    fileContent <- readFile $ homePath ++ "/.bashrc"
+    fileContent <- tryToOpenFile $ homePath ++ "/.bashrc"
     putStrLn fileContent
+    fileContentFromWrongPath <- tryToOpenFile $ homePath ++ "/.bashrc.wrong"
+    putStrLn fileContentFromWrongPath
+    fileContentFromWrongPath' <- tryToOpenFile' $ homePath ++ "/.bashrc.wrong"
+    putStrLn fileContentFromWrongPath'
