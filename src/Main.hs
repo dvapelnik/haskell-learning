1d24034c7b038c867e9a10071f4481814474cea1
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-01 23:32:07 +0200

    Maybe type is a Monad too

--- src/Main.hs
+++ src/Main.hs
@@ -3,29 +3,16 @@
 
 module Main where
 
-import Data.Char
+import Data.List
 import Data.Maybe
 
-coefficientFromString :: String -> Maybe Int
-coefficientFromString str =
-    if isNumber firstChar then Just (digitToInt firstChar) else Nothing
-    where firstChar = str !! 0
-
-check :: Maybe Int -> String
-check aCoefficient
-    | aCoefficient == Nothing = "Invalid string!"
-    | otherwise = show aCoefficient
-
-check' :: Maybe Int -> String
-check' aCoefficient =
-    if isNothing aCoefficient
-    then "Invalid string!"
-    else show aCoefficient
-
+result :: Maybe String -> String
+result email = if isNothing email then "Bad email" else "Good!"
 
 main :: IO ()
-main = do
-    print $ check $ coefficientFromString "0foo"
-    print $ check $ coefficientFromString "bar"
-    print $ check' $ coefficientFromString "0foo"
-    print $ check' $ coefficientFromString "bar"
+main = print $ result $ Just "me@gmail.com" >>= checkFormat >>= checkDomain
+    where
+        checkFormat email =
+          if '@' `elem` email then return email else Nothing
+        checkDomain email =
+          if ".com" `isSuffixOf` email then return email else Nothing
