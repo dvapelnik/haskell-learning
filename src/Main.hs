016a5713b3ff24823b69ff436715068b5718ddd6
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-01 23:12:38 +0200

    Maybe type

--- src/Main.hs
+++ src/Main.hs
@@ -4,32 +4,28 @@
 module Main where
 
 import Data.Char
+import Data.Maybe
 
---class Monad m where
---    (>>=)  :: m a -> (a -> m b) -> m b
---    (>>)   :: m a -> m b -> m b
---    return :: a -> m a
---    fail   :: String -> m a
+coefficientFromString :: String -> Maybe Int
+coefficientFromString str =
+    if isNumber firstChar then Just (digitToInt firstChar) else Nothing
+    where firstChar = str !! 0
 
---class Monad IO where
---    (>>=)  :: IO a -> (a -> IO b) -> IO b
---    (>>)   :: IO a -> IO b -> IO b
---    return :: a -> IO a
---    fail   :: String -> IO a
+check :: Maybe Int -> String
+check aCoefficient
+    | aCoefficient == Nothing = "Invalid string!"
+    | otherwise = show aCoefficient
 
-toLowerCase = return . toLower
-toRealNubmers = return . digitToInt
-doDouble = return . \n -> n + n
-addTen = return . \n -> n + 10
+check' :: Maybe Int -> String
+check' aCoefficient =
+    if isNothing aCoefficient
+    then "Invalid string!"
+    else show aCoefficient
 
-underlineSpaces char = return $ if char == ' ' then '_' else char
 
 main :: IO ()
 main = do
-    print $ text >>= toLowerCase >>= underlineSpaces
-    print $ numbers >>= toRealNubmers >>= doDouble >>= addTen
-    print $ justA >>= toLowerCase
-    where
-        text = "adadwdwadwda d awda wda "
-        numbers = "0123456789"
-        justA = Just 'A'
+    print $ check $ coefficientFromString "0foo"
+    print $ check $ coefficientFromString "bar"
+    print $ check' $ coefficientFromString "0foo"
+    print $ check' $ coefficientFromString "bar"
