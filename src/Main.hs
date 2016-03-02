b943bbcc9b7d5757595acb5a46b01e50af61e52f
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-02 22:32:26 +0200

    Functor example

--- src/Main.hs
+++ src/Main.hs
@@ -3,16 +3,24 @@
 
 module Main where
 
-import Data.List
-import Data.Maybe
+import Data.Functor -- for using <$>
 
-result :: Maybe String -> String
-result email = if isNothing email then "Bad email" else "Good!"
+--class Functor f where
+--    fmap :: (a -> b) -> f a -> f b
+
+data Year value = Year value deriving Show
+
+instance Functor Year where
+    fmap magicWand (Year value) = Year (magicWand value)
+
+increase :: Int -> Int
+increase val = val + 1
 
 main :: IO ()
-main = print $ result $ Just "me@gmail.com" >>= checkFormat >>= checkDomain
-    where
-        checkFormat email =
-          if '@' `elem` email then return email else Nothing
-        checkDomain email =
-          if ".com" `isSuffixOf` email then return email else Nothing
+main = do
+    print $ fmap increase iYear
+    print $ fmap (\year -> "Happy New " ++ year ++ " Year!") sYear
+    print $ (\year -> "Happy New " ++ year ++ " Year!") <$> sYear
+        where
+            iYear = Year 2015
+            sYear = Year "2015"
