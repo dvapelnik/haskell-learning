cea640594233652e1fb8ef7f719d87c3a726ad44
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-03 23:59:33 +0200

    Applicative functor: sequential application operator and pure function

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@
 module Main where
 
 import Data.Functor -- for using <$>
+import Control.Applicative
 
 --class Functor f where
 --    fmap :: (a -> b) -> f a -> f b
@@ -14,5 +15,17 @@ import Data.Functor -- for using <$>
 --    (*>)  :: f a -> f b -> f b
 --    (<*)  :: f a -> f b -> f a
 
+data Distance value = Distance value deriving Show
+
+instance Functor Distance where
+    fmap magicWand (Distance value) = Distance (magicWand value)
+
+instance Applicative Distance where
+    pure magicWand = Distance magicWand
+    Distance magicWand <*> functor = fmap magicWand functor
+
 main :: IO ()
 main = do
+    print $ (+) <$> Distance 3 <*> Distance 2
+    print $ pure (+) <*> Distance 3
+                     <*> Distance 2
