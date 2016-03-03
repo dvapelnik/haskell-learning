9097e6d0dcfeeab78378819c16247d9c1a3792cc
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-03 23:33:05 +0200

    Applicative functor declaring

--- src/Main.hs
+++ src/Main.hs
@@ -8,19 +8,11 @@ import Data.Functor -- for using <$>
 --class Functor f where
 --    fmap :: (a -> b) -> f a -> f b
 
-data Year value = Year value deriving Show
-
-instance Functor Year where
-    fmap magicWand (Year value) = Year (magicWand value)
-
-increase :: Int -> Int
-increase val = val + 1
+--class Functor f => Applicative f where
+--    pure  :: a -> f a
+--    (<*>) :: f (a -> b) -> f a -> f b  -- Наш главный герой.
+--    (*>)  :: f a -> f b -> f b
+--    (<*)  :: f a -> f b -> f a
 
 main :: IO ()
 main = do
-    print $ fmap increase iYear
-    print $ fmap (\year -> "Happy New " ++ year ++ " Year!") sYear
-    print $ (\year -> "Happy New " ++ year ++ " Year!") <$> sYear
-        where
-            iYear = Year 2015
-            sYear = Year "2015"
