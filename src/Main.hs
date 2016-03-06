c9aee6c0bbed87558f8cc906ff250098fbed150e
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-06 15:09:20 +0200

    Distance between points. Point is cartage

--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,7 @@ module Main where
 
 import Data.Char
 
-twoDigits2Int :: Char -> Char -> Int
-twoDigits2Int dec un =
-    if isDigit dec && isDigit un
-    then
-        digitToInt dec * 10 + digitToInt un
-    else
-        100
+dist :: (Double, Double) -> (Double, Double) -> Double
+dist p1 p2 = sqrt $ (fst(p2) - fst(p1))^2 + (snd(p2) - snd(p1))^2
 
-main = putStrLn . show $ twoDigits2Int '9' '5'
+main = putStrLn . show $ dist (1, 1) (1, 3)
