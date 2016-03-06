ce8b96bdea8b4f5e9e1b52a1e61b11eb380b54c2
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-06 17:12:30 +0200

    Fibonacci list generator

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,13 @@ module Main where
 
 import Data.Char
 
-doubleFact 0 = 1
-doubleFact 1 = 1
-doubleFact n = n * doubleFact (n - 2)
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci(n - 1) + fibonacci(n - 2)
 
+fibonacciList :: Int -> [Int]
+fibonacciList n | n == 0 = [0]
+                | n == 1 = [0, 1]
+fibonacciList n = fibonacciList (n - 1) ++ [fibonacci n]
 
-main = putStrLn . show . doubleFact $ 8
+main = putStrLn . show . fibonacciList $ 10
