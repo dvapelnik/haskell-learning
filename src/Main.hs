3cc8f2bb8ffc212a17e573f834da91d55e98699f
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-06 14:52:06 +0200

    Two chars to int ('9' '5' -> 95)

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,13 @@
 module Main where
 
+import Data.Char
 
+twoDigits2Int :: Char -> Char -> Int
+twoDigits2Int dec un =
+    if isDigit dec && isDigit un
+    then
+        digitToInt dec * 10 + digitToInt un
+    else
+        100
 
-main = do
-    putStrLn . show $ logBase 4 (min 20 (9 + 7))
-    putStrLn . show $ logBase 4 $ min 20 $ 9 + 7
+main = putStrLn . show $ twoDigits2Int '9' '5'
