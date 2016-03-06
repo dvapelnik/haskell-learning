6b97fe8031f1ff7188e7e79d32259d86146c09c7
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-06 17:28:33 +0200

    Fibonacci numbers with negative indeces

--- src/Main.hs
+++ src/Main.hs
@@ -2,13 +2,8 @@ module Main where
 
 import Data.Char
 
-fibonacci 0 = 0
-fibonacci 1 = 1
-fibonacci n = fibonacci(n - 1) + fibonacci(n - 2)
+fibonacci n | n == 0 || n == 1 = n
+            | n > 0 = fibonacci(n - 1) + fibonacci(n - 2)
+            | n < 0 = fibonacci(n + 2) - fibonacci(n + 1)
 
-fibonacciList :: Int -> [Int]
-fibonacciList n | n == 0 = [0]
-                | n == 1 = [0, 1]
-fibonacciList n = fibonacciList (n - 1) ++ [fibonacci n]
-
-main = putStrLn . show . fibonacciList $ 10
+main = putStrLn . show . fibonacci $ -10
