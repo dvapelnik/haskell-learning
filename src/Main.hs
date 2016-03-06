9b44dcb27212ac069b5519507ed158fad730fcac
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-06 20:24:24 +0200

    seqA: sequence: a0 = 1, a1 = 2, a2 = 3, a(k+3) = a(k+2) + a(k+1) - 2*a(k)

--- src/Main.hs
+++ src/Main.hs
@@ -2,14 +2,14 @@
 
 module Main where
 
-fibonacci n | n == 0  = 0
-            | n == 1  = 1
-            | n > 0   = goPositive n (0, 1)
-            | n < 0   = goNegative n (0, 1)
-        where
-            goPositive !n (!a, !b) | n==0      = a
-                                   | otherwise = goPositive (n-1) (b, a+b)
-            goNegative !n (!a, !b) | n==0      = a
-                                   | otherwise = goNegative (n+1) (b, a-b)
+seqA n | n == 0 = 1
+       | n == 1 = 2
+       | n == 2 = 3
+       | n >= 2 =
+            let
+                seqA' 0 p q r = r
+                seqA' n p q r = seqA' (n-1) q r (r+q-2*p)
+            in
+            seqA' (n-2) 1 2 3
 
-main = putStrLn . show . fibonacci $ -10
+main = putStrLn . show . seqA $ 301
