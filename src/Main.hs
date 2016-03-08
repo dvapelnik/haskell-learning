2f0eee095a53b99126651e10f66b2e34fadc9feb
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-08 13:52:25 +0200

    From compose function to (.) operator

--- src/Main.hs
+++ src/Main.hs
@@ -3,44 +3,13 @@
 
 module Main where
 
-import Data.Functor -- for using <$>
-import Control.Applicative
+-- (.) operator
 
---class Functor f where
---    fmap :: (a -> b) -> f a -> f b
-
---class Functor f => Applicative f where
---    pure  :: a -> f a
---    (<*>) :: f (a -> b) -> f a -> f b
---    (*>)  :: f a -> f b -> f b
---    (<*)  :: f a -> f b -> f a
-
-data User a = User a String deriving (Show)
-
-instance Functor User where
-    fmap f (User v1 v2) = User (f v1) v2
-
-data MyType value = MyType value deriving Show
-instance Functor MyType where
-    fmap fn (MyType value) = MyType ((\x -> x)(fn value))
-
-data Distance value = Distance value deriving Show
-
-instance Functor Distance where
-    fmap magicWand (Distance value) = Distance (magicWand value)
-
-instance Applicative Distance where
-    pure magicWand = Distance magicWand
-    Distance magicWand <*> functor = fmap magicWand functor
-    Distance a *> Distance b = Distance b
-    Distance a <* Distance b = Distance a
-
-obtainTwoTextsFromUser :: IO String
-obtainTwoTextsFromUser =
-    (joinWIthGlue "+++++") <$> getFirstText <*> getSecondText -- string concatenation
-    where getFirstText = putStrLn "Enter your text, please: " *> getLine
-          getSecondText = putStrLn "One more, please: " *> getLine
-          joinWIthGlue glue a b = a ++ glue ++ b
+compose f g x = f (g x)
 
 main :: IO ()
-main = obtainTwoTextsFromUser >>= \text -> putStrLn $ "Your glued strings is: " ++ text
+main = do
+    print $ compose (\x -> x + 10) (\x -> x - 2) 10
+    print $ compose (\x -> x + 10) (\x -> x - 2) $ 10
+    print $ (\x -> x + 10) `compose` (\x -> x - 2) $ 10
+    print $ (\x -> x + 10) . (\x -> x - 2) $ 10
