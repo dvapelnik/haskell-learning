fec0be342c75a6fa5f0ebf27d7b52ac086a09248
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-09 19:47:57 +0200

    Flip pair w/o swap function

--- src/Main.hs
+++ src/Main.hs
@@ -4,13 +4,5 @@ module Main where
 
 import Data.Function
 
-doItYourself = f . g . h
-
-f = logBase 2
-
-g = flip (^) 3
-
-h = max 42
-
 main = do
-    print $ doItYourself 4
+    print . show . (uncurry (flip (,))) $  (1,'A')
