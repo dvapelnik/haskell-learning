5f9ee688343f43ca6fa86480765f4cbe11fb4af1
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-09 19:31:36 +0200

    Chain of particaly applied function

--- src/Main.hs
+++ src/Main.hs
@@ -4,13 +4,13 @@ module Main where
 
 import Data.Function
 
---on :: (b -> b -> c) -> (a -> b) -> a -> a -> c
---on op f x y = f x `op` f y
+doItYourself = f . g . h
 
-on3 :: (b -> b -> b -> c) -> (a -> b) -> a -> a -> a -> c
-on3 op f x y z = op (f x) (f y) (f z)
+f = logBase 2
 
-sum3squares = (\x y z -> x+y+z) `on3` (^2)
+g = flip (^) 3
+
+h = max 42
 
 main = do
-    print $ sum3squares 1 2 3
+    print $ doItYourself 4
