2aa1720f8ef9707a72aa3381e48fcab340806170
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-15 21:04:49 +0200

    readDigits = span isDigit

--- src/Main.hs
+++ src/Main.hs
@@ -3,16 +3,11 @@
 
 module Main where
 
-groupElems :: Eq a => [a] -> [[a]]
-groupElems [] = []
-groupElems xs = groupElems' xs (head xs) [] [] where
-            groupElems' []     m p ls   = reverse (p:ls)
-            groupElems' (x:xs) m p ls | x== m = groupElems' xs x (x:p) ls
-                                      | otherwise = groupElems' xs x [x] (p:ls)
+import Data.Char
 
+readDigits :: String -> (String, String)
+readDigits = span isDigit
 
 main = do
---    print . show $ groupElems []
-    print . show $ groupElems [1,2]
-    print . show $ groupElems [1,2,2,2,4]
-    print . show $ groupElems [1,2,3,2,4]
+    print . show $ readDigits "365ads"
+    print . show $ readDigits "365"
