3df02c14e086adf6129b1dd40242dd8f87c9cdc9
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-15 23:56:47 +0200

    filter all uppercase words

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,10 @@
 
 module Main where
 
-perms            :: [a] -> [[a]]
-perms xs0        =  xs0 : perms' xs0 []
-  where
-    perms' []     _  = []
-    perms' (t:ts) is = foldr interleave (perms' ts (t:is)) (perms is)
-      where interleave    xs     r = let (_,zs) = interleave' id xs r in zs
-            interleave' _ []     r = (ts, r)
-            interleave' f (y:ys) r = let (us,zs) = interleave' (f . (y:)) ys r
-                                     in  (y:us, f (t:y:us) : zs)
+import Data.Char
+
+delAllUpper :: String -> String
+delAllUpper = unwords . filter (\s -> s /= map (\c -> toUpper c) s) . words
 
 main = do
-    print . show $ perms [0, 1, 2]
+    print . show $ delAllUpper "Abc IS not ABC"
