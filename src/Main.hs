b9cafb6727c74a0708c5dfd49eaed61057e155cd
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-17 01:03:07 +0200

    Enum Odd data type

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,17 @@
-
 module Main where
 
-import Prelude hiding (repeat)
+data Odd = Odd Integer
+    deriving (Eq, Show)
 
-repeat = iterate repeatHelper
-repeatHelper = \x -> x
+instance Enum Odd where
+    succ (Odd o) = Odd (o+2)
+    pred (Odd o) = Odd (o-2)
+    fromEnum (Odd a) = fromIntegral(a)
+    toEnum a = Odd (toInteger a)
+    enumFrom (Odd a) = map (\a -> Odd a) $ filter odd [a..]
+    enumFromThen (Odd a) (Odd b) = map (\a -> Odd a) $ filter odd [a,b..]
+    enumFromTo (Odd a) (Odd b) = map (\a -> Odd a) $ filter odd [a..b]
+    enumFromThenTo (Odd a) (Odd b) (Odd c)= map (\a -> Odd a) $ filter odd [a,b..c]
 
 main = do
-    print . show $ take 10 $ repeat 1
-    print . show $ take 10 $ repeat 'z'
-    print . show $ take 10 $ repeat "abc"
+    print . show $ succ $ Odd (-100000000000003)
