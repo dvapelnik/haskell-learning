4a8a96738ab23d15e47e0c55514c0e75620efadf
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-17 02:18:43 +0200

    List length via foldr

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,7 @@
 module Main where
 
-sumPositiveSquares :: [Integer] -> Integer
-sumPositiveSquares xs = foldr (\x y -> if x > 0 then x^2 + y else y) 0 xs
-
-sumPositiveSquares' :: [Integer] -> Integer
-sumPositiveSquares' xs = foldr f 0 xs
-    where
-        f x sum | x > 0     = x^2 + sum
-                | otherwise = sum
+lengthList :: [a] -> Int
+lengthList = foldr (\x s -> s + 1) 0
 
 main = do
-    print . show $ sumPositiveSquares  [1, -2, 2, 5, -10]
-    print . show $ sumPositiveSquares' [1, -2, 2, 5, -10]
+    print . show $ lengthList [7,6,5]
