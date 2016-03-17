7fa1346a81bd0a6b04124877fa1ffe182d80ab55
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-17 02:10:52 +0200

    Sum of positive squares

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,14 @@
 module Main where
 
-concatList :: [[a]] -> [a]
-concatList = foldr (++) []
+sumPositiveSquares :: [Integer] -> Integer
+sumPositiveSquares xs = foldr (\x y -> if x > 0 then x^2 + y else y) 0 xs
+
+sumPositiveSquares' :: [Integer] -> Integer
+sumPositiveSquares' xs = foldr f 0 xs
+    where
+        f x sum | x > 0     = x^2 + sum
+                | otherwise = sum
 
 main = do
-    print . show $ foldr (++) [] [[1,2],[],[3]]
-    print . show $ concatList [[1,2],[],[3]]
+    print . show $ sumPositiveSquares  [1, -2, 2, 5, -10]
+    print . show $ sumPositiveSquares' [1, -2, 2, 5, -10]
