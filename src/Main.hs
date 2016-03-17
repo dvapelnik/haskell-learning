d9bfc11dbdc9a2574d6a372ea0454b583302b905
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-17 02:06:56 +0200

    Concat list via foldr

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,8 @@
 module Main where
 
-coins :: (Num a) => [a]
-coins = [2, 3, 7]
-
-change :: (Ord a, Num a) => a -> [[a]]
-change n = if n < minimum coins then [] else change' n where
-	change' m  | m < minimum coins = [[]]
-		       | otherwise = [ xs | x <- coins, x <= m, xs <- map (\ys -> x:ys) $ change' (m - x), sum xs == m ]
+concatList :: [[a]] -> [a]
+concatList = foldr (++) []
 
 main = do
-    print . show $ change 7
-    print . show $ change 2
-    print . show $ change 10
+    print . show $ foldr (++) [] [[1,2],[],[3]]
+    print . show $ concatList [[1,2],[],[3]]
