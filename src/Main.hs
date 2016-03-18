056775d01ea71ca522132fddf0acd07f89b71fc2
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-19 00:17:05 +0200

    revRange of chars

--- src/Main.hs
+++ src/Main.hs
@@ -6,5 +6,19 @@ unfoldr f ini = helper (f ini)
         helper (Just(x, ini')) = x : unfoldr f ini'
         helper Nothing         = []
 
+revRange :: (Char,Char) -> [Char]
+revRange (a, b) =
+    let
+        g (x, (a, b)) =
+            if
+                x < a
+            then
+                Nothing
+            else
+                Just (x, (pred x, (a, b)))
+    in
+    unfoldr g (b, (a, b))
+
 main = do
     print . show $ unfoldr (\x -> if x == 10 then Nothing else Just (x, x+1)) 0
+    print . show $ revRange ('a','z')
