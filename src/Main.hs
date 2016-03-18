48e3b148c6cc700cd55714456cff9630801ba208
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-18 22:41:25 +0200

    Get last element of list using foldl1

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,10 @@
 module Main where
 
-evenOnly :: [a] -> [a]
-evenOnly = foldr (\(idx,x) a -> if even idx then x:a else a) [] . zip [1..]
+lastElem :: [a] -> a
+lastElem = foldl1 (\x y -> y)
 
 main = do
-    print . show $ evenOnly [1..10]
-    print . show $ evenOnly ['a'..'z']
-    print . show $ evenOnly [0]
-    print . show $ evenOnly [0, 1]
-    print . show $ take 5 . evenOnly $ [1..]
+    print . show $ lastElem [1..10]
+    print . show $ lastElem ['a'..'z']
+    print . show $ lastElem [0]
+    print . show $ lastElem [0, 1]
