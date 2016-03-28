dc2bd2d0639f78af782f7c56028c1c3df1434d43
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-28 20:17:48 +0300

    stringToColor

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,16 @@
 module Main where
 
-charToInt :: Char -> Int
-charToInt '0' = 0
-charToInt '1' = 1
-charToInt '2' = 2
-charToInt '3' = 3
-charToInt '4' = 4
-charToInt '5' = 5
-charToInt '6' = 6
-charToInt '7' = 7
-charToInt '8' = 8
-charToInt '9' = 9
+data Color = Red | Green | Blue
+
+instance Show Color where
+    show Red = "Red"
+    show Green = "Green"
+    show Blue = "Blue"
+
+stringToColor :: String -> Color
+stringToColor "Red" = Red
+stringToColor "Green" = Green
+stringToColor "Blue" = Blue
 
 main = do
     error "error"
