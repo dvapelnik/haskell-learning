3c7dea000bd5c71429f71e85fd252f3738d736df
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-28 20:43:25 +0300

    LogLEvel ordering

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,34 @@
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
+
 module Main where
 
-data Color = Red | Green | Blue
+data LogLevel = Error | Warning | Info
 
-instance Show Color where
-    show Red = "Red"
-    show Green = "Green"
-    show Blue = "Blue"
+instance Eq LogLevel where
+    Error == Error = True
+    Error == _ = False
+    Warning == Warning = True
+    Warning == _ = False
+    Info == Info = True
+    Info == _= False
+    _ == _ = False
 
-stringToColor :: String -> Color
-stringToColor "Red" = Red
-stringToColor "Green" = Green
-stringToColor "Blue" = Blue
+instance Ord LogLevel where
+    Error `compare` Warning = GT
+    Error `compare` Info = GT
+    Warning `compare` Info = GT
+    Info `compare` Warning = LT
+    Info `compare` Error = LT
+    Warning `compare` Error = LT
+    Error `compare` Error = EQ
+    Warning `compare` Warning = EQ
+    Info `compare` Info = EQ
+
+cmp :: LogLevel -> LogLevel -> Ordering
+cmp a b | a < b = LT
+        | a > b = GT
+        | a == b = EQ
 
 main = do
-    error "error"
+    print $ cmp Error Warning
+    print $ cmp Info  Warning
