8f1e869b0a8985a5db32fdbd8180b3ec04afdc57
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-28 22:31:03 +0300

    Bit numbers from https://goo.gl/OmnaZt

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,55 @@
 module Main where
 
-data Point = Point Double Double
+import Data.List (foldl', unfoldr)
 
-data Shape = Circle Double | Rectangle Double Double
+data Bit = O | I | Zero | One deriving (Eq, Show)
+data Sign = Minus | Plus deriving (Eq, Show)
+data Z = Z Sign [Bit] deriving (Eq, Show)
 
-area :: Shape -> Double
-area shape =
-    case shape of Circle r -> pi * r^2
-                  Rectangle a b -> a * b
+toInteger' :: Z -> Integer
+toInteger' (Z sign bits) = (decodeSign sign) * (fst $ decodeBits bits) where
+    decodeBits xs = foldl' f (0, 0) xs
+    f (num, pos) bit = (num + (decodeBit bit) * (2 ^ pos), pos + 1)
+    decodeBit O     = 0
+    decodeBit Zero  = 0
+    decodeBit I     = 1
+    decodeBit One   = 1
+    decodeSign Minus = -1
+    decodeSign Plus  =  1
 
-square :: Double -> Shape
-square a = Rectangle a a
 
-isSquare :: Shape -> Bool
-isSquare shape = case shape of Circle r -> False
-                               Rectangle a b -> a == b
+toZ :: Integer -> Z
+toZ num = Z (encodeSign num) (encodeBits $ abs num) where
+    -- unfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+    encodeBits num = unfoldr f num
+    f 0 = Nothing
+    f num = let (d, m) = divMod num 2 in Just (encodeBit m, d)
+    encodeSign num | num < 0   = Minus
+                   | otherwise = Plus
+    encodeBit 0 = Zero
+    encodeBit 1 = One
 
-main = do
-    undefined
+add :: Z -> Z -> Z
+add x y = toZ $ (toInteger' x) + (toInteger' y)
+mul :: Z -> Z -> Z
+mul x y = toZ $ (toInteger' x) * (toInteger' y)
+
+add' :: Z -> Z -> Integer
+add' x y = (toInteger' x) + (toInteger' y)
+mul' :: Z -> Z -> Integer
+mul' x y = (toInteger' x) * (toInteger' y)
+
+getZ1 = Z Plus [I,O,O,I]
+getZ2 = Z Minus [I,O,O,I]
+
+
+compute' :: ((Z -> Z -> Integer), [Char])  -> Z -> Z -> [Char]
+compute' (f, fName) x y = (show $ toInteger' x) ++ " " ++ fName ++ " " ++ (show $ toInteger' y) ++ " = " ++ (show $ (f x y))
+
+compute :: ((Z -> Z -> Z), [Char]) -> Z -> Z -> [Char]
+compute (f, fName) x y = (show $ toInteger' x) ++ " " ++ fName ++ " " ++ (show $ toInteger' y) ++ " = " ++ (show $ toInteger' (f x y))
+
+main = putStrLn $ show $ compute (mul,"*") getZ1 getZ2
+
+--main = do
+--    undefined
