a09bf0fa29b98d8842fa758900d313d4c6c6c047
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-28 20:02:10 +0300

    Char to int

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,16 @@
 module Main where
 
-data Color = Red | Green | Blue
-
-instance Show Color where
-    show Red = "Red"
-    show Green = "Green"
-    show Blue = "Blue"
+charToInt :: Char -> Int
+charToInt '0' = 0
+charToInt '1' = 1
+charToInt '2' = 2
+charToInt '3' = 3
+charToInt '4' = 4
+charToInt '5' = 5
+charToInt '6' = 6
+charToInt '7' = 7
+charToInt '8' = 8
+charToInt '9' = 9
 
 main = do
     error "error"
