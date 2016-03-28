6ee194e148bac491602c54a039ceac0c7015cf01
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-28 21:54:54 +0300

    Shape's area

--- src/Main.hs
+++ src/Main.hs
@@ -2,14 +2,12 @@ module Main where
 
 data Point = Point Double Double
 
-origin :: Point
-origin = Point 0.0 0.0
+data Shape = Circle Double | Rectangle Double Double
 
-distanceToOrigin :: Point -> Double
-distanceToOrigin (Point x y) = sqrt (x ^ 2 + y ^ 2)
-
-distance :: Point -> Point -> Double
-distance (Point x1 y1) (Point x2 y2) = sqrt((x1 - x2)^2 + (y1 - y2)^2)
+area :: Shape -> Double
+area shape =
+    case shape of Circle r -> pi * r^2
+                  Rectangle a b -> a * b
 
 main = do
     undefined
