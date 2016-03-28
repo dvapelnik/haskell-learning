a72192ad11b730e2c4bdbdfa0f11bded7928fcc5
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-28 23:05:27 +0300

    LogEntry

--- src/Main.hs
+++ src/Main.hs
@@ -1,55 +1,23 @@
 module Main where
 
-import Data.List (foldl', unfoldr)
+import Data.Time.Clock
+import Data.Time.Format
+import System.Locale
 
-data Bit = O | I | Zero | One deriving (Eq, Show)
-data Sign = Minus | Plus deriving (Eq, Show)
-data Z = Z Sign [Bit] deriving (Eq, Show)
+timeToString :: UTCTime -> String
+timeToString = formatTime defaultTimeLocale "%a %d %T"
 
-toInteger' :: Z -> Integer
-toInteger' (Z sign bits) = (decodeSign sign) * (fst $ decodeBits bits) where
-    decodeBits xs = foldl' f (0, 0) xs
-    f (num, pos) bit = (num + (decodeBit bit) * (2 ^ pos), pos + 1)
-    decodeBit O     = 0
-    decodeBit Zero  = 0
-    decodeBit I     = 1
-    decodeBit One   = 1
-    decodeSign Minus = -1
-    decodeSign Plus  =  1
+data LogLevel = Error | Warning | Info
 
+data LogEntry = LogEntry { timestamp :: UTCTime, logLevel :: LogLevel, message :: String }
 
-toZ :: Integer -> Z
-toZ num = Z (encodeSign num) (encodeBits $ abs num) where
-    -- unfoldr :: (b -> Maybe (a, b)) -> b -> [a]
-    encodeBits num = unfoldr f num
-    f 0 = Nothing
-    f num = let (d, m) = divMod num 2 in Just (encodeBit m, d)
-    encodeSign num | num < 0   = Minus
-                   | otherwise = Plus
-    encodeBit 0 = Zero
-    encodeBit 1 = One
+logLevelToString :: LogLevel -> String
+logLevelToString logLevel = case logLevel of Error -> "Error"
+                                             Warning -> "Warning"
+                                             Info -> "Info"
 
-add :: Z -> Z -> Z
-add x y = toZ $ (toInteger' x) + (toInteger' y)
-mul :: Z -> Z -> Z
-mul x y = toZ $ (toInteger' x) * (toInteger' y)
+logEntryToString :: LogEntry -> String
+logEntryToString logEntry = (timeToString . timestamp $ logEntry) ++ ": " ++ (logLevelToString . logLevel $ logEntry) ++ ": " ++ (message logEntry)
 
-add' :: Z -> Z -> Integer
-add' x y = (toInteger' x) + (toInteger' y)
-mul' :: Z -> Z -> Integer
-mul' x y = (toInteger' x) * (toInteger' y)
-
-getZ1 = Z Plus [I,O,O,I]
-getZ2 = Z Minus [I,O,O,I]
-
-
-compute' :: ((Z -> Z -> Integer), [Char])  -> Z -> Z -> [Char]
-compute' (f, fName) x y = (show $ toInteger' x) ++ " " ++ fName ++ " " ++ (show $ toInteger' y) ++ " = " ++ (show $ (f x y))
-
-compute :: ((Z -> Z -> Z), [Char]) -> Z -> Z -> [Char]
-compute (f, fName) x y = (show $ toInteger' x) ++ " " ++ fName ++ " " ++ (show $ toInteger' y) ++ " = " ++ (show $ toInteger' (f x y))
-
-main = putStrLn $ show $ compute (mul,"*") getZ1 getZ2
-
---main = do
---    undefined
+main = do
+    undefined
