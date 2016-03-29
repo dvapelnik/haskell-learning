487d4c7e2452bc3ba21a977776c717f03d7fce47
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-30 01:12:25 +0300

    Recursive natural numbers: add, mul and factorial

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,22 @@
 module Main where
 
-data List a = Nil | Cons a (List a) deriving Show
+data Nat = Zero | Suc Nat deriving Show
 
-fromList :: List a -> [a]
-fromList lst = case lst of Nil       -> []
-                           Cons x xs -> (x:fromList xs)
-toList :: [a] -> List a
-toList [] = Nil
-toList (x:xs) = x `Cons` (toList xs)
+fromNat :: Nat -> Integer
+fromNat Zero = 0
+fromNat (Suc n) = fromNat n + 1
+
+add :: Nat -> Nat -> Nat
+add  Zero   m = m
+add (Suc n) m = Suc (add n m)
+
+mul :: Nat -> Nat -> Nat
+mul  Zero   _ = Zero
+mul (Suc n) m = add m (mul n m)
+
+fac :: Nat -> Nat
+fac  Zero   = Suc Zero
+fac (Suc n) = mul (Suc n) (fac n)
 
 main = do
     undefined
