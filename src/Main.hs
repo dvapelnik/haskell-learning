b89b0177c43484f3d0c3fb3a7171ceca47abdf84
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-30 01:40:47 +0300

    Find avearage value of integer binary tree

--- src/Main.hs
+++ src/Main.hs
@@ -10,5 +10,19 @@ size :: Tree a -> Int
 size (Leaf l) = 1
 size (Node treeLeft treeRight) = 1 + size treeLeft + size treeRight
 
+avg :: Tree Int -> Int
+avg t =
+    let (c,s) = go t
+    in s `div` c
+  where
+    go :: Tree Int -> (Int,Int)
+    go (Leaf n) = (1, n)
+    go (Node treeLeft treeRight) =
+        let
+            (c1, s1) = go n1
+            (c2, s2) = go n2
+        in
+        (c1+c2, s1+s2)
+
 main = do
     undefined
