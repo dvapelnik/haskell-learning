909f95ddb24ce1b6b08ab4a79fbf5b950c2a95d3
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-29 23:41:19 +0300

    Find first dogit on string

--- src/Main.hs
+++ src/Main.hs
@@ -1,33 +1,17 @@
 module Main where
 
-import Data.Fixed
+import Data.Char(isDigit)
 
-data Coord a = Coord a a
-
-distance :: Coord Double -> Coord Double -> Double
-distance (Coord ax ay) (Coord bx by) = sqrt((ax - bx)^2 + (ay - by)^2)
-
-manhDistance :: Coord Int -> Coord Int -> Int
-manhDistance (Coord ax ay) (Coord bx by) = (abs $ ax - bx) + (abs $ ay - by)
-
---Плоскость разбита на квадратные ячейки.
---Стороны ячеек параллельны осям координат.
---Координаты углов ячейки с координатой (0,0) имеют неотрицательные координаты.
---Один из углов этой ячейки имеет координату (0,0).
---С ростом координат ячеек увеличиваются координаты точек внутри этих ячеек.
---Реализуйте функции getCenter, которая принимает координату ячейки и возвращает координату ее центра,
---и функцию getCell, которая принимает координату точки
---и возвращает номер ячейки в которой находится данная точка.
---В качестве первого аргумента обе эти функции принимают ширину ячейки.
-
-getCenter :: Double -> Coord Int -> Coord Double
-getCenter width (Coord cx cy) = Coord ((fromIntegral cx) * width + width/2) ((fromIntegral cy) * width + width/2)
-
-getCell :: Double -> Coord Double -> Coord Int
-getCell width (Coord xx xy) = Coord ix iy
+findDigit :: [Char] -> Maybe Char
+findDigit xs | xs == []  = Nothing
+             | otherwise = f xs
     where
-        (ix, rx) = divMod' xx width
-        (iy, ry) = divMod' xy width
+        f x'@(x:xs) | isDigit x = Just x
+                    | xs == []  = Nothing
+                    | otherwise = f xs
 
 main = do
-    undefined
+    print $ findDigit ""
+    print $ findDigit "qwer54"
+    print $ findDigit "45sadsd"
+    print $ findDigit "asdsdadasd"
