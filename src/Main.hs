0edf4e5f3f381d93010ec6e0ef2c6694a2a7e9b9
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-29 21:05:48 +0300

    getCenter and getCell

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Data.Fixed
+
 data Coord a = Coord a a
 
 distance :: Coord Double -> Coord Double -> Double
@@ -7,6 +9,25 @@ distance (Coord ax ay) (Coord bx by) = sqrt((ax - bx)^2 + (ay - by)^2)
 
 manhDistance :: Coord Int -> Coord Int -> Int
 manhDistance (Coord ax ay) (Coord bx by) = (abs $ ax - bx) + (abs $ ay - by)
+
+--Плоскость разбита на квадратные ячейки.
+--Стороны ячеек параллельны осям координат.
+--Координаты углов ячейки с координатой (0,0) имеют неотрицательные координаты.
+--Один из углов этой ячейки имеет координату (0,0).
+--С ростом координат ячеек увеличиваются координаты точек внутри этих ячеек.
+--Реализуйте функции getCenter, которая принимает координату ячейки и возвращает координату ее центра,
+--и функцию getCell, которая принимает координату точки
+--и возвращает номер ячейки в которой находится данная точка.
+--В качестве первого аргумента обе эти функции принимают ширину ячейки.
+
+getCenter :: Double -> Coord Int -> Coord Double
+getCenter width (Coord cx cy) = Coord ((fromIntegral cx) * width + width/2) ((fromIntegral cy) * width + width/2)
+
+getCell :: Double -> Coord Double -> Coord Int
+getCell width (Coord xx xy) = Coord ix iy
+    where
+        (ix, rx) = divMod' xx width
+        (iy, ry) = divMod' xy width
 
 main = do
     undefined
