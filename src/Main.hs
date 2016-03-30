4a2feb3fdfde15b95c022f0d40b39e5ac4603b14
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-30 23:38:15 +0300

    MapLike ListMap

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,48 @@
 module Main where
 
-import Data.Monoid
+--Ниже приведено определение класса MapLike типов, похожих на тип Map.
+--Определите представителя MapLike для типа ListMap, определенного ниже как список пар ключ-значение.
+--Для каждого ключа должно храниться не больше одного значения.
+--Функция insert заменяет старое значение новым, если ключ уже содержался в структуре.
 
-newtype Maybe' a = Maybe' { getMaybe :: Maybe a }
+import Prelude hiding (lookup)
+import qualified Data.List as L
+
+class MapLike m where
+    empty :: m k v
+    lookup :: Ord k => k -> m k v -> Maybe v
+    insert :: Ord k => k -> v -> m k v -> m k v
+    delete :: Ord k => k -> m k v -> m k v
+    fromList :: Ord k => [(k,v)] -> m k v
+    fromList [] = empty
+    fromList ((k,v):xs) = insert k v (fromList xs)
+
+newtype ListMap k v = ListMap { getListMap :: [(k,v)] }
     deriving (Eq,Show)
 
-instance Monoid a => Monoid (Maybe' a) where
-    mempty = Maybe'{getMaybe = (Just mempty)}
-    mappend mempty (Maybe' Nothing) = Maybe' Nothing
-    mappend (Maybe' Nothing) mempty = Maybe' Nothing
-    mappend (Maybe' a) (Maybe' b) = Maybe' (mappend a b)
+instance MapLike ListMap where
+    empty = ListMap{getListMap=[]}
+    lookup k table =
+        let
+            search_fn k [] = Nothing
+            search_fn k ((x,y):xs) | k == x = Just y
+                                   | otherwise = search_fn k xs
+        in
+        search_fn k (getListMap table)
+    insert k v table =
+        let
+		    insVal k v [] = [(k,v)]
+		    insVal k v ((x,y):xs) | k == x = ((k,v):xs)
+                                  | otherwise = (x,y):(insVal k v xs)
+        in
+        table{getListMap = (insVal k v (getListMap table))}
+    delete k table =
+        let
+            remKey k [] = []
+            remKey k xs'@((x,y):xs) | k == x = xs
+                                    | otherwise = ((x,y):(remKey k xs))
+        in
+        table{getListMap = (remKey k (getListMap table))}
 
 main = do
     undefined
