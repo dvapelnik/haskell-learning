04c0767cf4cf19136323270eadad680307305bfa
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-31 21:08:13 +0300

    Functor GeomPrimitive

--- src/Main.hs
+++ src/Main.hs
@@ -5,5 +5,11 @@ data Point3D a = Point3D a a a deriving Show
 instance Functor Point3D where
     fmap f (Point3D x y z) = Point3D (f x) (f y) (f z)
 
+data GeomPrimitive a = Point (Point3D a) | LineSegment (Point3D a) (Point3D a)
+
+instance Functor GeomPrimitive where
+    fmap f (Point p) = Point (fmap f p)
+    fmap f (LineSegment p1 p2) = LineSegment (fmap f p1) (fmap f p2)
+
 main = do
     undefined
