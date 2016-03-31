943aede1eaca6eac0a7663caed8210787968dba2
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-03-31 21:11:14 +0300

    Functor Tree

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,10 @@
 module Main where
 
-data Point3D a = Point3D a a a deriving Show
+data Tree a = Leaf (Maybe a) | Branch (Tree a) (Maybe a) (Tree a) deriving Show
 
-instance Functor Point3D where
-    fmap f (Point3D x y z) = Point3D (f x) (f y) (f z)
-
-data GeomPrimitive a = Point (Point3D a) | LineSegment (Point3D a) (Point3D a)
-
-instance Functor GeomPrimitive where
-    fmap f (Point p) = Point (fmap f p)
-    fmap f (LineSegment p1 p2) = LineSegment (fmap f p1) (fmap f p2)
+instance Functor Tree where
+    fmap f (Leaf m) = Leaf (fmap f m)
+    fmap f (Branch l m r) = Branch (fmap f l) (fmap f m) (fmap f r)
 
 main = do
     undefined
