f4bca2d2a5d3e2dd0396309d816a1d2847aac2c8
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-04-02 16:02:52 +0300

    Functor instancing using Monad

--- src/Main.hs
+++ src/Main.hs
@@ -1,31 +1,13 @@
 module Main where
 
-data Log a = Log [String] a deriving Show
+data SomeType a = SomeType a
 
-instance Monad Log where
-    return = returnLog
-    (>>=) = bindLog
+instance Monad SomeType where
+    return = SomeType
+    SomeType a >>= f = f a
 
-toLogger :: (a -> b) -> String -> (a -> Log b)
-toLogger f msg a = Log [msg] (f a)
-
-execLoggers :: a -> (a -> Log b) -> (b -> Log c) -> Log c
-execLoggers x f g =
-    let
-        Log ys y = f x
-        Log zs z = g y
-    in
-    Log (ys++zs) z
-
-returnLog :: a -> Log a
-returnLog a = Log [] a
-
-bindLog :: Log a -> (a -> Log b) -> Log b
-bindLog (Log logsa a) f = Log (logsa ++ logsb) b where
-  (Log logsb b) = f a
-
-execLoggersList :: a -> [a -> Log a] -> Log a
-execLoggersList = foldl (>>=) . return
+instance Functor SomeType where
+    fmap f x = x >>= \x' -> return (f x')
 
 main = do
     undefined
