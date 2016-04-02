3990be98599024aa1bc3ebe04ab6217d5ca87468
Dmitry Vapelnik <dvapelnik@gmail.com>
2016-04-02 21:50:09 +0300

    Name question

--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,9 @@
 module Main where
 
-import Data.Char
-
-import Control.Monad
-
-pythagoreanTriple :: Int -> [(Int, Int, Int)]
-pythagoreanTriple x | x <= 0 = []
-                    | otherwise = do
-                        c <- [1..x]
-                        b <- [1..x]
-                        a <- [1..b-1]
-                        True <- return(a^2 + b^2 == c^2)
-                        return (a, b, c)
-
+main :: IO ()
 main = do
-    undefined
+  putStr $ "What is your name?\nName: "
+  name <- getLine
+  if null name
+    then main
+    else putStrLn $ "Hi, " ++ name ++ "!"
